294eda1058d2554c42ebcc5ccf879db3926aef5d
Thien Chi Vi <meomeocf98@gmail.com>
2020-08-09 00:41:37 +0700

    Release project (#13) 🥰🥰🥰🥰
    * update: README
    
    * update: README
    
    * prepare: first release

---
+++ example/main.dart
@@ -0,0 +1,12 @@
+import 'package:mini_id/min_id.dart';
+
+void main() {
+  final id = MinId.getId();
+  print('ID:: $id');
+
+  final stringId = MinId.getId('3{w}-3{w}-3{w}');
+  print('ID:: $stringId');
+
+  final numId = MinId.getId('3{d}-3{w}-3{w}');
+  print('ID:: $numId');
+}
